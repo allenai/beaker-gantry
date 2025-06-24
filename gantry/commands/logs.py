e21d7460bee7f37ab8b1eb09e598e596f8e7e916
Pete Walsh <epwalsh10@gmail.com>
2025-06-24 16:13:58 -0700

    add --run option to `gantry logs` (#141)

--- gantry/commands/logs.py
+++ gantry/commands/logs.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 import click
-from beaker import BeakerJob
+from beaker import Beaker, BeakerJob, BeakerTask, BeakerWorkload
 from rich import print
 
 from .. import util
@@ -9,16 +9,32 @@ from ..exceptions import ConfigurationError
 from .main import CLICK_COMMAND_DEFAULTS, main
 
 
+def _get_job(
+    beaker: Beaker, wl: BeakerWorkload, task: BeakerTask, run: Optional[int] = None
+) -> Optional[BeakerJob]:
+    if run is None:
+        return beaker.workload.get_latest_job(wl, task=task)
+    else:
+        # NOTE: ascending sort order on creation time is not implemented server-side yet
+        jobs = list(reversed(list(beaker.job.list(task=task, sort_field="created"))))
+        try:
+            return jobs[run - 1]
+        except IndexError:
+            raise ConfigurationError(f"run number {run} does not exist")
+
+
 @main.command(**CLICK_COMMAND_DEFAULTS)
 @click.argument("workload", nargs=1, type=str)
 @click.option("-r", "--replica", type=int, help="""The replica rank to pull logs from.""")
 @click.option("--task", "task_name", type=str, help="""The name of task to pull logs from.""")
 @click.option("-t", "--tail", type=int, help="""Tail this many lines.""")
+@click.option("--run", type=int, help="""The run number to pull logs from.""")
 def logs(
     workload: str,
     replica: Optional[int] = None,
     task_name: Optional[int] = None,
     tail: Optional[int] = None,
+    run: Optional[int] = None,
 ):
     """
     Display the logs for an experiment workload.
@@ -26,14 +42,17 @@ def logs(
     if replica is not None and task_name is not None:
         raise ConfigurationError("--replica and --task are mutually exclusive")
 
+    if run is not None and run < 1:
+        raise ConfigurationError("--run must be at least 1")
+
     with util.init_client(ensure_workspace=False) as beaker:
         wl = beaker.workload.get(workload)
-        tasks = wl.experiment.tasks
+        tasks = list(wl.experiment.tasks)
 
         job: Optional[BeakerJob] = None
         if replica is not None:
             for task in tasks:
-                j = beaker.workload.get_latest_job(wl, task=task)
+                j = _get_job(beaker, wl, task, run=run)
                 if j is None:
                     print("[y]Experiment has not started yet[/]")
                     return
@@ -46,7 +65,7 @@ def logs(
         elif task_name is not None:
             for task in tasks:
                 if task.name == task_name:
-                    j = beaker.workload.get_latest_job(wl, task=task)
+                    j = _get_job(beaker, wl, task, run=run)
                     if j is None:
                         print("[y]Experiment has not started yet[/]")
                         return
@@ -56,7 +75,7 @@ def logs(
             else:
                 raise ConfigurationError(f"Invalid task name '{task_name}'")
         else:
-            job = beaker.workload.get_latest_job(wl)
+            j = _get_job(beaker, wl, tasks[0], run=run)
 
         if job is None:
             print("[y]Experiment has not started yet[/]")
